69fe133fb0b943fc652f6a3d35f15ab4170560e8
JohnCoronel <john.m.coronel@gmail.com>
2018-04-17 18:58:07 -0700

    redux implemented

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,14 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Fill in this function
+  console.log('increment fired')
+  return {
+    type: INCREMENT  
+  }
 };
 
 export const decrement = () => {
-  // Fill in this function
+  return {
+  type: DECREMENT
+  }
 };
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -17,13 +17,14 @@ class Counter extends Component {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
+        
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.increment()/* Fill me in */ }>
                     +
                 </button>
-                <button onClick={() => /* Fill me in */ }>
+                <button onClick={() => this.props.decrement()/* Fill me in */ }>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
